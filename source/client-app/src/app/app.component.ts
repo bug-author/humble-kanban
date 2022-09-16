1604452d1b5396f3c2fceec8a289d357e27404d3
Szymon Janecki <szjanecki@gmail.com>
2022-09-16 19:20:55 +0200

    feat: setup demo component to call an API

--- source/client-app/src/app/app.component.ts
+++ source/client-app/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+export interface TestObject {
+  body: string;
+}
+
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client-app';
+export class AppComponent implements OnInit {
+  public test = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<TestObject>('https://jsonplaceholder.typicode.com/posts/1')
+      .pipe(untilDestroyed(this))
+      .subscribe((data) => {
+        this.test =
+          data.body ===
+          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+            ? 'success'
+            : 'fail';
+      });
+  }
 }
