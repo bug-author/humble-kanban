2871fdeb354ea7e6a4821c12f6cb97228a6523ad
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-09-19 17:04:38 +0500

    add user model

---
+++ source/services/app/models.py
@@ -0,0 +1,20 @@
+import email
+from ssl import _PasswordType
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expresion import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    # ==== #
+    last_login = Column(TIMESTAMP(timezone=True))
+    is_active = Column(Boolean)
+    # ==== #
+    is_deleted = Column(Boolean, server_default='FALSE', nullable=False)
