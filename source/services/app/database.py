3cacefdc9ef94463f8e9f9caa8c0707980e6ef6d
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-09-20 19:20:00 +0500

    add orm set up

--- source/services/app/database.py
+++ source/services/app/database.py
@@ -1,3 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
+from .config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DB_URI = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_portnumber}/{settings.database_name}"
+
+engine = create_engine(SQLALCHEMY_DB_URI)
+
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
