25f8c8c88ba02c4f6590eb6f487107d0de97eb39
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-09-21 06:53:16 +0500

    update username to display_name

--- source/services/app/models.py
+++ source/services/app/models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    username = Column(String, nullable=False)
+    display_name = Column(String, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
 
