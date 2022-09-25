ed5189b9118753ff6d7f55590e1d68f7fc2570cb
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-09-25 15:03:37 +0500

    add projects model

--- source/services/app/models.py
+++ source/services/app/models.py
@@ -14,8 +14,21 @@ class Users(Base):
 
     # ==== #
     last_login = Column(TIMESTAMP(timezone=True))
-    is_active = Column(Boolean, server_default='TRUE')
-    is_deleted = Column(Boolean, server_default='FALSE', nullable=False)
+    is_active = Column(Boolean, server_default="TRUE")
+    is_deleted = Column(Boolean, server_default="FALSE", nullable=False)
 
     created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
+                        nullable=False, server_default=text("now()"))
+
+
+class Projects(Base):
+    __tablename__ = "projects"
+
+    project_id = Column(BigInteger, primary_key=True, nullable=False)
+    project_name = Column(String, nullable=False)
+    project_creator = ...
+    project_start_date = Column(TIMESTAMP(timezone=True),
+                                nullable=False, server_default=text("now()"))
+    project_end_date = Column(TIMESTAMP(timezone=True),
+                              nullable=False)
+    board_id = ...
