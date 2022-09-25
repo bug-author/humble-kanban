9932f07717d97bda6e62e8f304a87cf980b44058
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-09-25 15:27:31 +0500

    add board_creator_id FK in boards

--- source/services/app/models.py
+++ source/services/app/models.py
@@ -41,7 +41,8 @@ class Boards(Base):
     board_id = Column(BigInteger, primary_key=True, nullable=False)
     board_name = Column(String, nullable=False)
     project_id = ...
-    board_creator_id = ...
+    board_creator_id = Column(BigInteger, ForeignKey(
+        "users.user_id", ondelete="CASCADE"), nullable=False)
 
 
 class Tickets(Base):
