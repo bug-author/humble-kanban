1da255b6905b892b876c155d07cdda0a26278751
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-10-16 16:04:33 +0500

    rename class to stay consistent with docs

--- source/services/app/database.py
+++ source/services/app/database.py
@@ -7,13 +7,13 @@ SQLALCHEMY_DB_URI = f"postgresql://{settings.database_username}:{settings.databa
 
 engine = create_engine(SQLALCHEMY_DB_URI)
 
-LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 def get_db():
-    db = LocalSession()
+    db = SessionLocal()
     try:
         yield db
     finally:
