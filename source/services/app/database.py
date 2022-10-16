eea311a85583d814c485f49aded239f98d734446
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-10-16 16:03:54 +0500

    add dependency for independent session per request

--- source/services/app/database.py
+++ source/services/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from config import settings
+from .config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -10,3 +10,11 @@ engine = create_engine(SQLALCHEMY_DB_URI)
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = LocalSession()
+    try:
+        yield db
+    finally:
+        db.close()
