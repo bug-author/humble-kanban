4f357dbfc6d53886fa48125e305d28a6e7813a4a
Abdullah <48406637+bug-author@users.noreply.github.com>
2022-12-10 16:22:54 +0500

    add password hashing when creating user

--- source/services/app/routers/user.py
+++ source/services/app/routers/user.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, status, Response, APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import models, data_transfer_objects as dto
+from .. import models, data_transfer_objects as dto, utilities
 
 router = APIRouter(
     prefix='/users',
@@ -12,10 +12,15 @@ router = APIRouter(
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=dto.UsersOut)
 def create_user(user: dto.UsersCreate, db: Session = Depends(get_db)):
+
+    hashed_password = utilities.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.Users(**user.dict())
     db.add(new_user)
     db.commit()
     # get an updated version of object for current session
     db.refresh()
 
+    # this will return the new_user as per the structure of UsersOut dto
     return new_user
